25d1af9a347d6c3b88bd1d604f4cd43b66b0faec
Noémie Taillandier <taillandier.n@hotmail.fr>
2015-04-24 00:47:30 +0200

    Contenu de l'index

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-﻿
+﻿<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<title>Eranann</title>
+<link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="menu">
+<?php include'menu.html';?>
+</div> <!-- Menu de gauche -->
+<div id="header">
+<?php include'titrehead.html';?>
+</div> <!-- boite de titre -->
+<div id="intro">
+<h1>Site en construction !!!</h1>
+<h1>Vous venez de passer les portes des trois Royaumes, êtes-vous prêt(e) à entrer dans la légende ? </h1>
+
+<p>Eranann, Varuna et Sidh sont trois royaumes reliés entre eux par une magie défaillante, Sidh et Eranann n'ont presque plus de contact et Varuna est 
+aux abonnés absents depuis des siècles, les aventures qui suivent vont nous plonger dans l'histoire mythique de ces trois mondes.</br>
+</br>
+Alors si vous êtes prêt(e), Bienvenue en Eranann, voyageur !</p>
+</div> <!-- Corps de texte -->
+<div id="pied">
+<p>www.eranann.fr Copyright</p>
+</div> <!-- Pied de page -->
+</body>
+<html>
